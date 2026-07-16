a614e2fb64765d7f10694fe946de11e8a193584b
alephgabriel52-prog <alephgabriel52@gmail.com>
2026-07-16 16:42:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'PARADOX RP ONLINE: {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong! Bot 24/7 online ✅')
+
+bot.run(os.getenv('TOKEN'))
 
