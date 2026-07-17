18f9a257f2b1cbf9260dda4bf3a6f8852b7c6606
alephgabriel52-prog <alephgabriel52@gmail.com>
2026-07-17 08:30:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 from flask import Flask
 import threading
 
-# 1. SERVIDOR PRA NÃO DORMIR
 app = Flask('')
 
 @app.route('/')
@@ -18,7 +17,6 @@ def keep_alive():
     t = threading.Thread(target=run)
     t.start()
 
-# 2. SEU BOT
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='!', intents=intents)
@@ -27,6 +25,6 @@ client = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'PARADOX RP ONLINE: {client.user}')
 
-# 3. LIGA TUDO JUNTO
-keep_alive() # liga o site primeiro
-client.run(os.getenv('TOKEN')) # liga o bot depois
+if __name__ == "__main__":
+    keep_alive()
+    client.run(os.getenv('TOKEN'))
